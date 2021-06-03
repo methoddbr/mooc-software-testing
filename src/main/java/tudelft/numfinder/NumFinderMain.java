60bff0a3a66a9b35f3ad5649d883a5d8a8b39cb2
Leonardo Prestes <methoddbr@gmail.com>
2021-06-03 19:55:13 -0300

    corrected numfinder class ans test tomannumber class

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -6,9 +6,10 @@ public class NumFinderMain {
         NumFinder nf = new NumFinder();
 
         // this works
-        // nf.find(new int[] {4, 25, 7, 9});
+        //nf.find(new int[] {4, 25, 7, 9});
 
-        // this crashes
+        // this crashes *corrected*
+        //nf.find(new int[] {1000, 45645, 3434, 23534, 6});
         nf.find(new int[] {4, 3, 2, 1});
 
         System.out.println(nf.getLargest());

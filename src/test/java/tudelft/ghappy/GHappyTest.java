cc946da3faa14736bf4b335722947ba16a7bbd0c
Leonardo Prestes <methoddbr@gmail.com>
2021-06-10 22:09:48 -0300

    created tests for ghappy and mirror classes

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,13 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import tudelft.leapyear.LeapYear;
+
 public class GHappyTest {
+    @Test
+    public void isHappy() {
+        GHappy ha = new GHappy();
+        Assertions.assertTrue(ha.gHappy("leggo"));
+    }
 }
--- src/test/java/tudelft/mirror/MirrorTest.java
+++ src/test/java/tudelft/mirror/MirrorTest.java
@@ -1,4 +1,13 @@
 package tudelft.mirror;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MirrorTest {
+    @Test
+    public void mirrorEndTest() {
+        Mirror mi = new Mirror();
+        String result = mi.mirrorEnds("asdfasdfasdfsdfsdfsdfsdfsdfsdfsdfsdfsdfsdfdsfasdfdsa");
+        Assertions.assertEquals("asdf", result);
+    }
 }
